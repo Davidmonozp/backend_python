8400b28619de59e31086579000ac015b7c6fd867
Davidmonozp <130225723+Davidmonozp@users.noreply.github.com>
2026-05-19 00:47:46 -0500

    Configurar CORS para permitir peticiones desde localhost

--- core/settings.py
+++ core/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -152,3 +153,9 @@ SIMPLE_JWT = {
     'ALGORITHM': 'HS256',
     'AUTH_HEADER_TYPES': ('Bearer',), 
 }
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173", 
+]
+
